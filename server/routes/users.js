6fb72a65a7e432ea56ed7c423209f2f1408c7a62
Jameel <sbijjj1@gmail.com>
2024-10-24 17:56:57 +0800

    Implemented user routes changes, schema, and models

---
+++ server/routes/users.js
@@ -0,0 +1,34 @@
+import express from 'express';
+import User from '../models/user.js';
+
+const router = express.Router();
+
+router.post('/users', async (req, res) => {
+    const user = req.body;
+    
+    //TODO : Create a user in the database
+    const result = await User.create(user)
+    return res.status(201).json(result);
+});
+
+router.get('/users/:id', async (req, res) => {
+    const id = req.params.id;
+
+    let errorMessage = null;
+
+    try{
+        const user = await User.findById(id);
+        if(user){
+            return res.json(user);
+        }
+
+        errorMessage = 'User not found';
+    }
+    catch(error){
+        errorMessage = 'User not found or Invalid ID';  
+    }
+
+    return res.status(404).json({error: errorMessage});
+});    
+
+export default router;
